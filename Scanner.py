a00b59733bf86c52731e5a0fd8dbaab7b7e77fdd
Rajarshi Mondal <rajarshidgamer@gmail.com>
2023-07-21 14:31:55 +0530

    added box

--- Scanner.py
+++ Scanner.py
@@ -10,6 +10,9 @@ while True:
     if decode(img):
         for code in decode(img):
             print(code.data.decode("utf-8"))
+            points = np.array([code.polygon],np.int32)
+            points.reshape((-1,1,2))
+            cv2.polylines(img,[points],True,(255,0,255),2)
 
     img = cv2.flip(img,1)
     cv2.imshow("Screan", img)
