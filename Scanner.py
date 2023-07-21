129f32fd499a47a05b61102613248d74d021d8af
Rajarshi Mondal <rajarshidgamer@gmail.com>
2023-07-21 14:08:36 +0530

    main file

--- Scanner.py
+++ Scanner.py
@@ -2,11 +2,22 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-img = cv2.imread('qr_code.png')
+#img = cv2.imread('qr_code.png')
 
+cam = cv2.VideoCapture(0)
 
-for code in decode(img):
-    print(code.data.decode("utf-8"))
+while True:
+    _, img = cam.read()
+    if decode(img):
+        for code in decode(img):
+            print(code.data.decode("utf-8"))
+
+    img = cv2.flip(img,1)
+    cv2.imshow("Screan", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
 
 
 
